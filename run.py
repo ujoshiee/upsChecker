8e7a1b39d49a81bf80a24fb9f0850d5de5954871
root <root@c.jmiao.net>
2017-04-17 16:31:20 -0700

    initial

---
+++ run.py
@@ -0,0 +1,18 @@
+import requests
+from config import *
+import time
+import os
+
+shipped = 0
+while shipped == 0:
+	try:
+		r = requests.get('https://wwwapps.ups.com/WebTracking/processInputRequest?loc=en_US&Requester=MCDP&tracknum=' + trackNum + '&returnTo=https://wwwapps.ups.com/mcdp?loc=en_US')
+		if (r.text.find('Lousville')>0):
+			print('found')
+			os.system(curlString)
+			shipped = 1
+		else:
+			print('not found')
+	except requests.exceptions.RequestException as e:
+		print(e)
+	time.sleep(60)
